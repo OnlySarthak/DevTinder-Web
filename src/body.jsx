4f1ac996ddef08cb929acfcb3e30eb2aa3bb8f0e
OnlySarthak <devsarthak.hotline@gmail.com>
2025-07-10 00:53:20 +0530

    Initial routing

---
+++ src/body.jsx
@@ -0,0 +1,12 @@
+// import { Outlet } from 'react-router-dom';
+import NavBar from './navbar';
+
+const body = () => {
+  return (
+    <div>
+        <NavBar/>
+    </div>
+  )
+}
+
+export default body
